65e1dd78122f87fb9eeffc148510d5b681e9dbcb
Israel Joab <israeljoab@gmail.com>
2024-02-24 15:52:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# workspace_java_lab
+ This repository is a valuable source of practical exercises, code examples, and challenging projects designed to enhance my Java programming skills.
---
+++ java_lab/src/com/izzydev/javalab01/VariaveisInteiras.java
@@ -0,0 +1,19 @@
+package com.izzydev.javalab01;
+
+public class VariaveisInteiras {
+
+	public static void main(String[] args) {
+
+		byte idade01 = 20;
+		
+		short idade02 = 21;
+		
+		int idade03 = 22;
+		
+		long idade04 = 24;
+		
+		System.out.println("Valor Variável idade01 = " + idade01);
+
+	}
+
+}
